5a1eef7aca171397056cfeb0cb6e0c21d3fca116
Christian Barkowsky <hallo@christianbarkowsky.de>
2015-05-18 15:52:19 +0200

    A lot of improvements

--- classes/TinyCompressImages.php
+++ classes/TinyCompressImages.php
@@ -15,95 +15,35 @@ namespace Barkowsky;
 
 class TinyCompressImages extends \System
 {
-	
 	/**
-	 *
+	 * Compress images
 	 */
 	public function processPostUpload($arrFiles)
 	{
 		if (is_array($arrFiles) && $GLOBALS['TL_CONFIG']['tinypng_api_key'] != '') {
+			
+			$strUrl = 'https://api.tinypng.com/shrink';
+			$strKey = $GLOBALS['TL_CONFIG']['tinypng_api_key'];
+			$strAuthorization = 'Basic '.base64_encode("api:$strKey");
+			
 			foreach($arrFiles as $file) {
 				
-				$input = TL_ROOT . '/' . $file;
-				$output = TL_ROOT . '/' . $file;
+				$strFile = TL_ROOT . '/' . $file;
 				
-				if (extension_loaded('curl')) {	
-					$request = curl_init();
-					
-					curl_setopt_array($request, array(
-						CURLOPT_URL => "https://api.tinypng.com/shrink",
-						CURLOPT_USERPWD => "api:" . $GLOBALS['TL_CONFIG']['tinypng_api_key'],
-						CURLOPT_POSTFIELDS => file_get_contents($input),
-						CURLOPT_BINARYTRANSFER => true,
-						CURLOPT_RETURNTRANSFER => true,
-						CURLOPT_HEADER => true,
-						//Uncomment below if you have trouble validating our SSL certificate.
-						//Download cacert.pem from: http://curl.haxx.se/ca/cacert.pem
-						// CURLOPT_CAINFO => __DIR__ . "/cacert.pem",
-						CURLOPT_SSL_VERIFYPEER => false
-					));
-					
-					$response = curl_exec($request);
-					
-					if (curl_getinfo($request, CURLINFO_HTTP_CODE) === 201) {
-						
-						// Compression was successful, retrieve output from Location header.
-						$headers = substr($response, 0, curl_getinfo($request, CURLINFO_HEADER_SIZE));
-						
-						foreach (explode("\r\n", $headers) as $header) {
-							if (strtolower(substr($header, 0, 10)) === "location: ") {
-								$request = curl_init();
-								
-								curl_setopt_array($request, array(
-									CURLOPT_URL => substr($header, 10),
-									CURLOPT_RETURNTRANSFER => true,
-									// Uncomment below if you have trouble validating our SSL certificate.
-									// CURLOPT_CAINFO => __DIR__ . "/cacert.pem",
-									CURLOPT_SSL_VERIFYPEER => false
-								));
-								
-								file_put_contents($output, curl_exec($request));
-								
-								\Contao\System::log('Compression was successful. (File: ' . $file . ')', __METHOD__, TL_GENERAL);
-							}
-						}					
-					} else {
-						\Contao\System::log('Compression failed. (' . curl_error($request) . ') (File: ' . $file . ')', __METHOD__, TL_GENERAL);
-					}
+				$objRequest = new Request();
+				$objRequest->method = 'post';
+				$objRequest->data = file_get_contents($strFile);
+				$objRequest->setHeader('Content-type', 'image/png');
+				$objRequest->setHeader('Authorization', $strAuthorization);
+				$objRequest->send($strUrl);
+				
+				$arrResponse = json_decode($objRequest->response);
+				
+				if ($objRequest->code == 201) {
+					file_put_contents($strFile, fopen($arrResponse->output->url, "rb", false));
+					\Contao\System::log('Compression was successful. (File: ' . $file . ')', __METHOD__, TL_GENERAL);
 				} else {
-					$url = "https://api.tinypng.com/shrink";
-					
-					$options = array(
-						"http" => array(
-							"method" => "POST",
-							"header" => array(
-								"Content-type: image/png",
-								"Authorization: Basic " . base64_encode("api:" . $GLOBALS['TL_CONFIG']['tinypng_api_key'] . "")
-							),
-							"content" => file_get_contents($input)
-						),
-						"ssl" => array(
-							//Uncomment below if you have trouble validating our SSL certificate.
-							//Download cacert.pem from: http://curl.haxx.se/ca/cacert.pem
-							// "cafile" => __DIR__ . "/cacert.pem",
-							"verify_peer" => false
-						)
-					);
-					
-					$result = fopen($url, "r", false, stream_context_create($options));
-					
-					if ($result) {
-						// Compression was successful, retrieve output from Location header.
-						foreach ($http_response_header as $header) {
-							if (strtolower(substr($header, 0, 10)) === "location: ") {
-								file_put_contents($output, fopen(substr($header, 10), "rb", false));
-								
-								\Contao\System::log('Compression was successful. (File: ' . $file . ')', __METHOD__, TL_GENERAL);
-							}
-						}
-					} else {
-						\Contao\System::log('Compression failed. (' . curl_error($request) . ') (File: ' . $file . ')', __METHOD__, TL_GENERAL);
-					}
+					\Contao\System::log('Compression failed. (' . $arrResponse->message . ') (File: ' . $file . ')', __METHOD__, TL_GENERAL);
 				}
 			}
 		}
