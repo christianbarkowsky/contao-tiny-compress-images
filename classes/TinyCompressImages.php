0fbe1f88a51342160d064b7de7f9634351649e01
Christian Barkowsky <mail@christianbarkowsky.de>
2015-05-18 21:28:28 +0200

    Code improvements

--- classes/TinyCompressImages.php
+++ classes/TinyCompressImages.php
@@ -26,25 +26,34 @@ class TinyCompressImages extends \System
 			$strKey = $GLOBALS['TL_CONFIG']['tinypng_api_key'];
 			$strAuthorization = 'Basic '.base64_encode("api:$strKey");
 			
-			foreach($arrFiles as $file) {
+			foreach($arrFiles as $file) {	
+				$objFile = \FilesModel::findByPath($file);
 				
-				$strFile = TL_ROOT . '/' . $file;
-				
-				$objRequest = new \Request();
-				$objRequest->method = 'post';
-				$objRequest->data = file_get_contents($strFile);
-				$objRequest->setHeader('Content-type', 'image/png');
-				$objRequest->setHeader('Authorization', $strAuthorization);
-				$objRequest->send($strUrl);
-				
-				$arrResponse = json_decode($objRequest->response);
-				
-				if ($objRequest->code == 201) {
-					file_put_contents($strFile, fopen($arrResponse->output->url, "rb", false));
-					\Dbafs::addResource($file);
-					\Contao\System::log('Compression was successful. (File: ' . $file . ')', __METHOD__, TL_GENERAL);
-				} else {
-					\Contao\System::log('Compression failed. (' . $arrResponse->message . ') (File: ' . $file . ')', __METHOD__, TL_GENERAL);
+				if (in_array($objFile->extension, array('png', 'jpg', 'jpeg'))) {
+					
+					$strFile = TL_ROOT . '/' . $file;
+					
+					$objRequest = new \Request();
+					$objRequest->method = 'post';
+					$objRequest->data = file_get_contents($strFile);
+					$objRequest->setHeader('Content-type', 'image/png');
+					$objRequest->setHeader('Authorization', $strAuthorization);
+					$objRequest->send($strUrl);
+					
+					$arrResponse = json_decode($objRequest->response);
+					
+					if ($objRequest->code == 201) {
+						file_put_contents($strFile, fopen($arrResponse->output->url, "rb", false));
+						
+						$objFile->tstamp = time();
+						$objFile->path   = $file;
+						$objFile->hash   = md5_file(TL_ROOT . '/' . $file);
+						$objFile->save();
+						
+						\Contao\System::log('Compression was successful. (File: ' . $file . ')', __METHOD__, TL_FILES);
+					} else {
+						\Contao\System::log('Compression failed. (' . $arrResponse->message . ') (File: ' . $file . ')', __METHOD__, TL_FILES);
+					}
 				}
 			}
 		}
